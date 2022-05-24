5a1de8ba645cfcf2947011dba582cb61a38a0bea
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2022-05-24 23:34:03 +0600

    Update internet_connection_checker.py

--- internet_connection_checker.py
+++ internet_connection_checker.py
@@ -1,15 +1,30 @@
 import urllib.request
+import RPi.GPIO as GPIO
+import time
 
-def connect(host='http://google.com'):
-    try:
-        urllib.request.urlopen(host)
-        return True
-    except:
-        return False
+flag = 0
 
-print( "connected" if connect() else "no internet!" )
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
-if connect() == True:
-    print("ok")
-elif connect() == False:
-    print("false")
+while True:
+    def connect(host='http://google.com'):
+        try:
+            urllib.request.urlopen(host)  # getting a ping response
+            return True
+        except:
+            flag+= 1
+            return False
+
+    if connect() == True and flag == 0:
+        GPIO.output(18, GPIO.LOW)
+        print("Everything is ok 😘")
+    elif connect() == False or flag != 0:
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(18, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(18, GPIO.HIGH)
+        print("Connection broke unga bunga 🐒🐵")
+    
+    GPIO.cleanup()  # clean up or resets ports what I've used
